0449e560760821e4249042dce8b7ba3525f16280
Sr.Lechuga <jonattanlima@gmail.com>
2023-07-31 21:03:20 -0300

    Selection logic and style

--- code/script.js
+++ code/script.js
@@ -1,1 +1,27 @@
 "use strict";
+const modal = document.querySelector(".modal-overlay"),
+      language = localStorage.getItem("language");
+
+const closeModal = () =>{
+    modal.style.animation = "disappear 1s forwards";
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 1000);
+};
+
+const selectLanguage = () =>{
+    document.querySelector(".english").addEventListener("click",() =>{
+        localStorage.setItem("language","en");
+        closeModal();
+    });
+    document.querySelector(".spanish").addEventListener("click",() =>{
+        localStorage.setItem("language","es");
+        closeModal();
+    });
+};
+
+if (language === null) selectLanguage();
+else {
+    console.log(`Language: ${language}`);
+    modal.style.display = "none";
+}
